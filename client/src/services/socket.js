85f142fcc46e8c1ba82328feb8b68561e765e551
Santhosh Kunam <kunamsanthosh992@gmail.com>
2025-09-15 20:15:51 +0530

    Updated cors links

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,2 +1,2 @@
 import { io } from 'socket.io-client';
-export const socket = io('https://link-space-it1cn8t6n-kunamsanthosh994-cpus-projects.vercel.app');
+export const socket = io('https://linkspace-lxa7.onrender.com');
